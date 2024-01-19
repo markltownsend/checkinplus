c814f2ce55e2746eb0c2fe205b0dbcfca137ce28
Mark Townsend <markltownsend@users.noreply.github.com>
2024-01-19 12:00:46 -0800

    Add serviceID to the FoursquareAPI protocol

--- FoursquareAPI/Sources/FoursquareAPI/FoursquareAPI.swift
+++ FoursquareAPI/Sources/FoursquareAPI/FoursquareAPI.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 public protocol FoursquareAPI {
+    nonisolated static var serviceID: String { get }
     nonisolated func authorizeUser(_ callbackURI: String) throws
     func generateAuthToken(with url: URL, callbackURI: String) async throws -> String?
     func getCheckInVenues(latitude: Double, longitude: Double) async throws -> [Venue]?
